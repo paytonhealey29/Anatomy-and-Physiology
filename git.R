5e289c1655edf2d7a90c58daec1f32d1b911d4fb
paytonhealey <he1191832@ccbcmd.email.edu>
2026-03-24 15:39:43 +0000

    lab notebook

--- git.R
+++ git.R
@@ -1,6 +1,6 @@
 usethis::use_git_config(
-  user.name = "paytonhealey",
-  user.email = "he1191832@ccbcmd.email.edu",
+  user.name = "maptv",
+  user.email = "mlaptev@ccbcmd.edu",
 )
 
 usethis::create_github_token()
